44314f1adf75edb1dc23b0db6c6ff9939ae83ee1
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-01-11 16:56:43 +0200

    Compute the floor of a float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Basic Annotations Example: Computing the floor of a float.
+"""
+
+import math
+
+
+def compute_floor(n: float) -> int:
+    """
+    Computes the floor of a given float number.
+
+    Parameters:
+        float_number (float): The float for which the floor value needs to be computed.
+    Returns:
+        int: The floor value of the input float.
+    """
+    return math.floor(n)
