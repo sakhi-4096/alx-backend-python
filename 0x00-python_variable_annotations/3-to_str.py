333c15c374725b5281a60455b80d63af0b24e329
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-01-11 16:57:07 +0200

    Convert a float to its string representation.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Basic Annotations Example: Converting a float to its string representation.
+"""
+
+def float_to_string(n: float) -> str:
+    """
+    Converts a float to its string representation.
+
+    Parameters:
+        float_number (float): The float to be converted to a string.
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
