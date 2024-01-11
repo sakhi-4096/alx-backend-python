95d87f598ef646fa0967317200145e1f91dd5ebe
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-01-11 16:56:15 +0200

    Add module showcasing basic variable annotations for string concatenation.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+    Simple module showcasing basic variable annotations for string concatenation.
+"""
+
+
+def concatenate_strings(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+        first_string (str): The first string to be concatenated.
+        second_string (str): The second string to be concatenated.
+    Returns:
+        str: The result of concatenating the two input strings.
+    """
+    return str1 + str2
