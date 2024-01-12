ca5fdf4a2dab46aadce02dae7f1a6fb8c502b302
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2024-01-12 22:17:53 +0200

    Zoom into a Tuple with a specified factor.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,7 +7,7 @@ Type Checking Example: Zooming into a Tuple with a specified factor.
 from typing import Tuple, List
 
 
-def zoom_array(source_tuple: Tuple, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zooms into a Tuple by repeating its elements based on a specified factor.
 
@@ -19,7 +19,7 @@ def zoom_array(source_tuple: Tuple, factor: int = 2) -> List:
         List: A List containing the zoomed-in elements.
     """
     zoomed_in: List = [
-        item for item in source_tuple
+        item for item in lst
         for _ in range(factor)
     ]
     return zoomed_in
