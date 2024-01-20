272549dbf2a249c477f23ec67c8551103f78043b
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2024-01-20 05:02:42 +0200

    Add executable permissions.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-'''Import async_comprehension from the previous file and write a measure_runtime
-coroutine that will execute async_comprehension four times in parallel using
-asyncio.gather.
+'''Import async_comprehension from the previous file and write a
+measure_runtime coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather.
 '''
 import asyncio
 import time
