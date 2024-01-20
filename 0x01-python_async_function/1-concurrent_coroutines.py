e9cdb536507796c4145d344b5f7f3a856c1b681f
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2024-01-20 04:58:55 +0200

    Remove trailing whitespace (line 4).

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''Import wait_random from the previous python file that you’ve written
 and write an async routine called wait_n that takes in 2 int arguments
-(in this order): n and max_delay. 
+(in this order): n and max_delay.
 '''
 import asyncio
 from typing import List
